44f50abb4d49f799bcceef12e964cb03db78228a
NyEstevo <alineestevo_s4@hotmail.com>
2026-09-07 10:58:39 -0300

    fix(docs): remove comentário do backend de prod

--- fase-3/infra/terraform/envs/prod/backend.tf
+++ fase-3/infra/terraform/envs/prod/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    # Backend criado por fase-3/infra/bootstrap/prod (conta pessoal).
     bucket         = "tc-fiap-tfstate-prod"
     key            = "fase-3/prod/terraform.tfstate"
     region         = "us-east-1"
